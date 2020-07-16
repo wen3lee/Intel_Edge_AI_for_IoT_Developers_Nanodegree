50c8066ae4847218a82b65e014b08ed50c3a3360
Ann Lee <wen3lee@gmail.com>
2020-07-16 13:40:46 +0800

    Pooling performance exercise
    Task 1: Calculate Model FLOPs
    Task 2: Completing the Inference Pipeline

---
+++ Exercise/Pooling_Performance/inference.py
@@ -0,0 +1,32 @@
+from openvino.inference_engine import IENetwork, IECore
+
+import numpy as np
+import time
+
+# Getting model bin and xml file
+model_path='pool_cnn/pool_cnn'
+model_weights=model_path+'.bin'
+model_structure=model_path+'.xml'
+
+# TODO: Load the model
+# Use either IECore or IEPlugin API
+core = IECore()
+model = IENetwork(model_structure, model_weights)
+net = core.load_network(network=model, device_name='CPU')
+
+input_name=next(iter(net.inputs))
+
+# Reading and Preprocessing Image
+input_img=np.load('image.npy')
+input_img=input_img.reshape(1, 28, 28)
+
+input_dict={input_name:input_img}
+
+# TODO: Using the input image, run inference on the model for 10 iterations
+start=time.time()
+
+for _ in range(10):
+    net.infer(input_dict)
+
+# TODO: Finish the print statement
+print("Time taken to run 10 iterations is:{}".format(time.time()-start))
