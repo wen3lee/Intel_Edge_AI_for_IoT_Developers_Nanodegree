4e94a8b1024e54cf600bb35c415b6fc6cbee6286
Ann Lee <wen3lee@gmail.com>
2020-07-17 09:08:20 +0800

    Measuring layerwise performance exercise
    Task 1: Finish perf_counts.py file and load the sep_cnn model to find the layerwise performance of the model.
    Task 2: Load the pool_cnn model using the same script and compare the performance of the two models.

---
+++ Exercise/Exercise_Measuring_Layerwise_Performance/perf_counts.py
@@ -0,0 +1,43 @@
+import numpy as np
+from openvino.inference_engine import IENetwork
+from openvino.inference_engine import IEPlugin
+
+import pprint
+import argparse
+import sys
+
+def main(args):
+    pp = pprint.PrettyPrinter(indent=4)
+    model=args.model
+    device=args.device
+    image_path=args.image
+
+    # Loading model
+    model_weights=model+'.bin'
+    model_structure=model+'.xml'
+    
+    # TODO: Load the model
+    model=IENetwork(model_structure, model_weights)
+    plugin = IEPlugin(device=device)
+
+    # Loading network to device
+    net = plugin.load(network=model, num_requests=1)
+    
+    input_name=next(iter(model.inputs))
+    
+    # Reading and Preprocessing Image
+    input_img=np.load(image_path)
+    input_img=input_img.reshape(1, 28, 28)
+
+    # TODO: Run Inference and print the layerwise performance
+    net.requests[0].infer({input_name:input_img})
+    pp.pprint(net.requests[0].get_perf_counts())
+    
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--model', required=True)
+    parser.add_argument('--device', default='CPU')
+    parser.add_argument('--image', default=None)
+    
+    args=parser.parse_args()
+    sys.exit(main(args) or 0)
